e872efc7ec049cdeb83cf9202a074a308dd43f17
邓捷 <dj@lifesdk.com>
2021-12-22 18:12:07 +0800

    implement functions

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Cyvadra/horus-btc
 
 go 1.16
+
+require (
+	github.com/NpoolPlatform/go-service-framework v0.0.0-20211207121121-adb2402676f0
+	github.com/client9/misspell v0.3.4 // indirect
+	github.com/json-iterator/go v1.1.12
+)
--- main.go
+++ main.go
@@ -1,1 +1,42 @@
-package horusbtc
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/Cyvadra/horus-btc/structures"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	jsoniter "github.com/json-iterator/go"
+)
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+func main() {
+	scanner, err := readSampleFile("./sample/txRow-json.txt")
+	if err != nil {
+		logger.Sugar().Error(err)
+		return
+	}
+	parseData(scanner)
+}
+
+func readSampleFile(path string) (scanner *bufio.Scanner, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return bufio.NewScanner(file), nil
+}
+
+func parseData(scanner *bufio.Scanner) {
+	jsonCache := &structures.TransactionRow{}
+	for i := 0; i < 3; i++ {
+		scanner.Scan()
+		if err := json.Unmarshal(scanner.Bytes(), jsonCache); err != nil {
+			logger.Sugar().Error(err)
+		}
+		fmt.Println(jsonCache)
+	}
+}
