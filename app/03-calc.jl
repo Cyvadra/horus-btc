dba33a3226cdfca0801a94b5be42a6878fb48260
邓捷 <dj@lifesdk.com>
2022-02-09 14:51:23 +0800

    Add feature: AddressSnapshot

--- app/03-calc.jl
+++ app/03-calc.jl
@@ -161,6 +161,47 @@ include("./02-loadmmap.jl")
 		end
 	AddressDiffTpl = ThreadSafeDict{UInt32,Float64}()
 
+# Address Snapshot
+	mutable struct AddressSnapshot
+		# for statistics
+		BalanceVector::Vector{Float64}
+		TsActiveVector::Vector{Int32}
+		# in profit
+		NumInProfitRealized::Int64
+		AmountInProfitRealized::Float64
+		NumInProfitUnrealized::Int64
+		AmountInProfitUnrealized::Float64
+		# in loss
+		NumInLossRealized::Int64
+		AmountInLossRealized::Float64
+		NumInLossUnrealized::Int64
+		AmountInLossUnrealized::Float64
+		end
+	tplAddressSnapshot = AddressSnapshot(Float64[], Int32[], zeros(length(AddressSnapshot.types)-2)...)
+	function TakeSnapshot()::AddressSnapshot
+		ret = deepcopy(tplAddressSnapshot)
+		for p in AddressState
+			push!(ret.BalanceVector, p[2].Balance)
+			push!(ret.TsActiveVector, p[2].TimestampLastActive)
+			if p[2].UsdtNetRealized > 0
+				ret.NumInProfitRealized += 1
+				ret.AmountInProfitRealized += p[2].UsdtNetRealized
+			elseif p[2].UsdtNetRealized < 0
+				ret.NumInLossRealized += 1
+				ret.AmountInLossRealized -= p[2].UsdtNetRealized
+			end
+			if p[2].UsdtNetUnrealized > 0
+				ret.NumInProfitUnrealized += 1
+				ret.AmountInProfitUnrealized += p[2].UsdtNetUnrealized
+			elseif p[2].UsdtNetUnrealized < 0
+				ret.NumInLossUnrealized += 1
+				ret.AmountInLossUnrealized -= p[2].UsdtNetUnrealized
+			end
+		end
+		return ret
+		end
+
+
 # Runtime Statistics
 	mutable struct PeriodStat
 		timestamp::Int32
