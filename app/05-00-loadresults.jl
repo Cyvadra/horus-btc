d6e5c80d1cdc78b4eebb3403ecb036a83b6af349
邓捷 <dj@lifesdk.com>
2022-02-13 15:46:14 +0800

    Add dataloader(x)(calculated results); added method to trans them into Vector

---
+++ app/05-00-loadresults.jl
@@ -0,0 +1,111 @@
+
+	mutable struct ResultCalculations
+		timestamp::Int32
+		# CellAddressComparative
+		numTotalActive::Int32
+		numTotalRows::Int32
+		amountTotalTransfer::Float32
+		percentBiasReference::Float32
+		percentNumNew::Float32
+		percentNumSending::Float32
+		percentNumReceiving::Float32
+		# CellAddressDirection
+		numChargePercentBelow10::Int32
+		numChargePercentBelow25::Int32
+		numChargePercentBelow50::Int32
+		numChargePercentBelow80::Int32
+		numChargePercentBelow95::Int32
+		numChargePercentEquals100::Int32
+		numWithdrawPercentBelow10::Int32
+		numWithdrawPercentBelow25::Int32
+		numWithdrawPercentBelow50::Int32
+		numWithdrawPercentAbove80::Int32
+		numWithdrawPercentAbove95::Int32
+		amountChargePercentBelow10::Float32
+		amountChargePercentBelow25::Float32
+		amountChargePercentBelow50::Float32
+		amountChargePercentBelow80::Float32
+		amountChargePercentBelow95::Float32
+		amountChargePercentEquals100::Float32
+		amountWithdrawPercentBelow10::Float32
+		amountWithdrawPercentBelow25::Float32
+		amountWithdrawPercentBelow50::Float32
+		amountWithdrawPercentAbove80::Float32
+		amountWithdrawPercentAbove95::Float32
+		# CellAddressAccumulation
+		numRecentD3Sending::Int32
+		numWakeupW1Sending::Int32
+		numWakeupM1Sending::Int32
+		numRecentD3Buying::Int32
+		numWakeupW1Buying::Int32
+		numWakeupM1Buying::Int32
+		numContinuousD1Sending::Int32
+		numContinuousD3Sending::Int32
+		numContinuousW1Sending::Int32
+		numContinuousD1Buying::Int32
+		numContinuousD3Buying::Int32
+		numContinuousW1Buying::Int32
+		amountRecentD3Sending::Float32
+		amountWakeupW1Sending::Float32
+		amountWakeupM1Sending::Float32
+		amountRecentD3Buying::Float32
+		amountWakeupW1Buying::Float32
+		amountWakeupM1Buying::Float32
+		amountContinuousD1Sending::Float32
+		amountContinuousD3Sending::Float32
+		amountContinuousW1Sending::Float32
+		amountContinuousD1Buying::Float32
+		amountContinuousD3Buying::Float32
+		amountContinuousW1Buying::Float32
+		# CellAddressSupplier
+		balanceSupplierMean::Float32
+		balanceSupplierStd::Float32
+		balanceSupplierPercent20::Float32
+		balanceSupplierPercent40::Float32
+		balanceSupplierMiddle::Float32
+		balanceSupplierPercent60::Float32
+		balanceSupplierPercent80::Float32
+		balanceSupplierPercent95::Float32
+		amountSupplierBalanceBelow20::Float32
+		amountSupplierBalanceBelow40::Float32
+		amountSupplierBalanceBelow60::Float32
+		amountSupplierBalanceBelow80::Float32
+		amountSupplierBalanceAbove95::Float32
+		# CellAddressUsdtDiff
+		numRealizedProfit::Int32
+		numRealizedLoss::Int32
+		amountRealizedProfitBillion::Float64
+		amountRealizedLossBillion::Float64
+		end
+	_types = Vector{DataType}(collect(ResultCalculations.types))
+	_names = string.(collect(fieldnames(ResultCalculations)))
+	_len   = length(_types)
+
+#= Data Pretreatment
+		fieldname starts with:
+			timestamp
+				ignore
+			num
+				use directly
+			amount
+				log2()
+			percent
+				use directly
+=#
+
+	need_log2 = findall(x->x[1:3]=="amo", _names)
+	function result2vector(res::ResultCalculations)::Vector{Float32}
+		ret = Vector{Float32}(undef, _len-1)
+		for i in 2:_len
+			if i in need_log2
+				ret[i-1] = log2(getfield(res,i))
+			else
+				ret[i-1] = getfield(res,i)
+			end
+		end
+		return ret
+		end
+
+
+
+
