cfa8bbfc7e5d148a18e4e12f8e4d4e9c36613c21
邓捷 <dj@lifesdk.com>
2022-02-13 15:16:33 +0800

    Rewrite dnn.jl, add data-loader, add feature definition

--- app/05-dnn.jl
+++ app/05-dnn.jl
@@ -1,32 +1,55 @@
 using Flux
 using DataFrames
+using FinanceDB
 
-inputSize     = nrow(df)-1 # ignore timestamp
 
-stepPrev  = 10 # ticks
-stepNext  = 1  # predict which
+# Params
+	includePrev = 10 # ticks
+	stepsNext   = Int[1,2,4,8,16] # predict next n status
 
-tsFirst   = max(df[1,:timestamp], d.TsFirst)
-tsEnd     = min(df[end,:timestamp], d.TsEnd)
-numTicks  = floor(Int, (tsEnd-tsFirst)/d.TsInterval)
+# Feature definition
+	struct GonnaHappen
+		ChangedPercents::Float32
+		HighestPercents::Float32
+		LowestPercents::Float32
+		NextOpenPercents::Float32
+		end
 
-dfStartRow = findfirst(x->x>=tsFirst, df.timestamp)
-dfEndRow   = findlast(x->x!==0 && x<=tsEnd, df.timestamp)
-deStartRow = findfirst(x->x>=tsFirst, d.Timestamps[])
-deEndRow   = findlast(x->x!==0 && x<=tsEnd, d.Timestamps[])
+# Load market data
+	FinanceDB.SetDataFolder("/mnt/data/mmap")
+	d = FinanceDB.GetDerivative("BTC_USDT");
+	df = DataFrame(
+		timestamp = d.Timestamps[],
+		open = d.Open[],
+		high = d.High[],
+		low  = d.Low[],
+		close = d.Close[],
+		vol   = d.Volume[]
+		);
+	df = df[1:findfirst(x->iszero(x), d.Timestamps[])-1, :];
+	df[!,:mid] = (2df.open + df.close + 3df.high + 3df.low) ./ 9;
+	GC.gc()
+
+# Definition of Y
+#=
+	OHLC of $stepsNext tick
+=#
+	function GenerateYAtRowIAfterStepsN(i::Int, n::Int)::GonnaHappen
+		currentPrice = df[i,:mid]
+		return GonnaHappen(
+			df[i+n, :mid] / currentPrice - 1.0,
+			max(df[i:i+n, :high]...) / currentPrice - 1.0,
+			min(df[i:i+n, :low]...) / currentPrice - 1.0,
+			df[i+n, :open] / currentPrice - 1.0,
+			)
+		end
 
 
 # Definition of X
 #=
-	Previous $stepPrev data, auto-configure weight
+	Previous $includePrev data, auto-configure weight
 =#
 
-
-
-# Definition of Y
-#=
-	OHLC of $stepNext tick
-=#
 
 
 modelWidth    = 512
