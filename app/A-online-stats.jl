22c59880f9a46613c2f37bc56a6e66d94d7b9450
邓捷 <dj@lifesdk.com>
2022-02-26 17:05:30 +0800

    Add function BlockNum2Timestamp

--- app/A-online-stats.jl
+++ app/A-online-stats.jl
@@ -13,6 +13,7 @@ include("./service-address2id.jl");
 # Config
 dataFolder   = "/mnt/data/cacheTx/"
 shuffleRng   = Random.MersenneTwister(10086)
+tsFile       = "/mnt/data/bitcore/BlockTimestamps.dict.jld2"
 
 # Init Mongo
 client = Mongoc.Client("mongodb://localhost:27017")
@@ -98,13 +99,12 @@ function GetBlockCoins(height::Int)::Vector{Mongoc.BSON}
 	return retList
 	end
 
-
+# Transfer into Vector{cacheTx}
 struct cacheTx
 	addressId::UInt32
 	amount::Float64
 	timestamp::Int32
 	end
-# Union: Update Results
 function ProcessBlockN(height::Int)::Vector{cacheTx}
 	txs           = GetBlockTransactions(height)
 	timeStamp     = round(Int, datetime2unix(txs[1]["blockTime"]))
@@ -143,7 +143,66 @@ function ProcessBlockN(height::Int)::Vector{cacheTx}
 	return retList
 	end
 
+# BlockNum 2 Timestamp
+BlockTimestamps = JLD2.load(tsFile)["BlockTimestamps"]
+function BlockNum2Timestamp(height::Int)::Int32
+	if haskey(BlockTimestamps, height)
+		return BlockTimestamps[height]
+	end
+	BlockTimestamps[height] = round(Int32, 
+		datetime2unix(
+			GetBlockInfo(height)["timeNormalized"]
+		)
+	)
+	return BlockTimestamps[height]
+	end
+function SyncBlockTimestamps()
+	JLD2.save(tsFile, "BlockTimestamps", BlockTimestamps)
+	return nothing
+	end
+atexit(SyncBlockTimestamps)
 
-
+# Realtime Address Service
+mutable struct AddressStatistics
+	# timestamp
+	TimestampCreated::Int32
+	TimestampLastActive::Int32
+	TimestampLastReceived::Int32
+	TimestampLastPayed::Int32
+	# amount
+	AmountIncomeTotal::Float64
+	AmountExpenseTotal::Float64
+	# statistics
+	NumTxInTotal::Int32
+	NumTxOutTotal::Int32
+	# relevant usdt amount
+	UsdtPayed4Input::Float64
+	UsdtReceived4Output::Float64
+	AveragePurchasePrice::Float32
+	LastSellPrice::Float32
+	# calculated extra
+	UsdtNetRealized::Float64
+	UsdtNetUnrealized::Float64
+	Balance::Float64
+	end
+function Address2State(addr::String)
+	coins = GetCoinsByAddress(addr)
+	ret   = AddressStatistics(
+		0, # TimestampCreated Int32
+		0, # TimestampLastActive Int32
+		0, # TimestampLastReceived Int32
+		0, # TimestampLastPayed Int32
+		0, # AmountIncomeTotal Float64
+		0, # AmountExpenseTotal Float64
+		0, # NumTxInTotal Int32
+		0, # NumTxOutTotal Int32
+		0, # UsdtPayed4Input Float64
+		0, # UsdtReceived4Output Float64
+		0, # AveragePurchasePrice Float32
+		0, # LastSellPrice Float32
+		0, # UsdtNetRealized Float64
+		0, # UsdtNetUnrealized Float64
+		0, # Balance Float64
+		)
 
 
