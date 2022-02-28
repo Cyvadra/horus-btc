9a6902496cdc1a7ae1a06d6eabca85ed075312df
邓捷 <dj@lifesdk.com>
2022-02-28 17:24:03 +0800

    Feature: MergeAddressState

--- app/A-online-stats.jl
+++ app/A-online-stats.jl
@@ -220,6 +220,7 @@ mutable struct AddressDiff
 	UsdtReceived4Output::Float64
 	LastSellPrice::Float32
 	end
+tplAddressDiff       = AddressDiff(zeros(length(AddressDiff.types))...)
 tplAddressStatistics = AddressStatistics(zeros(length(AddressStatistics.types))...)
 function Address2State(addr::String, blockNum::Int)::AddressStatistics
 	blockNum += 1
@@ -294,7 +295,7 @@ function Address2State(addr::String, blockNum::Int)::AddressStatistics
 		ret.UsdtNetUnrealized = ret.Balance * (currentPrice - ret.AveragePurchasePrice)
 	return ret
 	end
-function Address2StateDiff(addr::String, fromBlock::Int, toBlock::Int)::AddressStatistics
+function Address2StateDiff(addr::String, fromBlock::Int, toBlock::Int)::AddressDiff
 	fromBlock -= 1
 	toBlock   += 1
 	coins = Mongoc.find(
@@ -312,22 +313,8 @@ function Address2StateDiff(addr::String, fromBlock::Int, toBlock::Int)::AddressS
 	mintNums  = map(x->x["mintHeight"], coins)
 	spentNums = map(x->x["spentHeight"], coins[spentRange])
 	blockNums = sort!(vcat(mintNums, spentNums))
-	ret       = deepcopy(tplAddressStatistics)
-	if length(mintNums) > 0 && length(spentNums) > 0
-		ret.TimestampCreated = blockNums[1] |> BlockNum2Timestamp # Int32
-		ret.TimestampLastActive = blockNums[end] |> BlockNum2Timestamp # Int32
-		ret.TimestampLastReceived = mintNums[end] |> BlockNum2Timestamp # Int32
-		ret.TimestampLastPayed = spentNums[end] |> BlockNum2Timestamp # Int32
-		ret.AmountIncomeTotal = map(x->x["value"],
-			coins
-		) |> sum |> bitcoreInt2Float64 # Float64
-		ret.AmountExpenseTotal = map(x->x["value"],
-			coins[spentRange]
-		) |> sum |> bitcoreInt2Float64 # Float64
-		ret.NumTxInTotal = length(mintNums) # Int32
-		ret.NumTxOutTotal = length(spentNums) # Int32
-	elseif length(blockNums) > 0
-		ret.TimestampLastActive = blockNums[end] |> BlockNum2Timestamp
+	ret       = deepcopy(tplAddressDiff)
+	if length(blockNums) > 0
 		if length(mintNums) > 0
 			ret.TimestampLastReceived = mintNums[end] |> BlockNum2Timestamp
 			ret.AmountIncomeTotal = map(
@@ -345,8 +332,6 @@ function Address2StateDiff(addr::String, fromBlock::Int, toBlock::Int)::AddressS
 			ret.NumTxOutTotal = length(spentNums)
 		end
 	end
-	# Balance
-		ret.Balance = ret.AmountIncomeTotal - ret.AmountExpenseTotal
 	# LastSellPrice
 		if length(spentNums) > 0
 			ret.LastSellPrice = FinanceDB.GetDerivativePriceWhen( pairName, BlockNum2Timestamp(spentNums[end]) )
@@ -364,12 +349,52 @@ function Address2StateDiff(addr::String, fromBlock::Int, toBlock::Int)::AddressS
 				coins[spentRange]
 			) |> sum
 		end
-		if abs(ret.Balance) > 0.00
-			ret.AveragePurchasePrice = ret.UsdtPayed4Input / ret.AmountIncomeTotal
-		end
-	# UsdtNetRealized / UsdtNetUnrealized
-		ret.UsdtNetRealized = ret.UsdtReceived4Output - ret.UsdtPayed4Input
-		ret.UsdtNetUnrealized = GetPriceAtBlockN(toBlock)
 	return ret
 	end
-
+function MergeAddressState!(baseState::AddressStatistics, arrayDiff::Vector{AddressDiff}, coinPrice::Float32)::AddressStatistics
+	for d in arrayDiff
+		if d.TimestampLastReceived > 0
+			baseState.TimestampLastReceived = d.TimestampLastReceived
+			baseState.AmountIncomeTotal += d.AmountIncomeTotal
+			baseState.NumTxInTotal += d.NumTxInTotal
+			baseState.UsdtPayed4Input += d.UsdtPayed4Input
+		end
+		if d.TimestampLastPayed > 0
+			baseState.TimestampLastPayed = d.TimestampLastPayed
+			baseState.AmountExpenseTotal += d.AmountExpenseTotal
+			baseState.NumTxOutTotal += d.NumTxOutTotal
+			baseState.UsdtReceived4Output += d.UsdtReceived4Output
+			baseState.LastSellPrice = d.LastSellPrice
+		end
+	end
+	baseState.TimestampLastActive = max(baseState.TimestampLastReceived, baseState.TimestampLastPayed)
+	baseState.AveragePurchasePrice = baseState.UsdtPayed4Input / baseState.AmountIncomeTotal
+	baseState.UsdtNetRealized = baseState.UsdtReceived4Output - baseState.UsdtPayed4Input
+	baseState.Balance = baseState.AmountIncomeTotal - baseState.AmountExpenseTotal
+	baseState.UsdtNetUnrealized = baseState.Balance * (coinPrice - ret.AveragePurchasePrice)
+	return baseState
+	end
+function MergeAddressState(baseState::AddressStatistics, arrayDiff::Vector{AddressDiff}, coinPrice::Float32)::AddressStatistics
+	baseState = deepcopy(baseState)
+	for d in arrayDiff
+		if d.TimestampLastReceived > 0
+			baseState.TimestampLastReceived = d.TimestampLastReceived
+			baseState.AmountIncomeTotal += d.AmountIncomeTotal
+			baseState.NumTxInTotal += d.NumTxInTotal
+			baseState.UsdtPayed4Input += d.UsdtPayed4Input
+		end
+		if d.TimestampLastPayed > 0
+			baseState.TimestampLastPayed = d.TimestampLastPayed
+			baseState.AmountExpenseTotal += d.AmountExpenseTotal
+			baseState.NumTxOutTotal += d.NumTxOutTotal
+			baseState.UsdtReceived4Output += d.UsdtReceived4Output
+			baseState.LastSellPrice = d.LastSellPrice
+		end
+	end
+	baseState.TimestampLastActive = max(baseState.TimestampLastReceived, baseState.TimestampLastPayed)
+	baseState.AveragePurchasePrice = baseState.UsdtPayed4Input / baseState.AmountIncomeTotal
+	baseState.UsdtNetRealized = baseState.UsdtReceived4Output - baseState.UsdtPayed4Input
+	baseState.Balance = baseState.AmountIncomeTotal - baseState.AmountExpenseTotal
+	baseState.UsdtNetUnrealized = baseState.Balance * (coinPrice - ret.AveragePurchasePrice)
+	return baseState
+	end
