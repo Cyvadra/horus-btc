19ee2eb588006f56b25b8e2af5512a2cbb30af30
邓捷 <dj@lifesdk.com>
2022-03-03 17:54:35 +0800

    basic calculations

--- app/03-01-generate_state.jl
+++ app/03-01-generate_state.jl
@@ -1,6 +1,73 @@
 
 using ProgressMeter
 
+
+mutable struct AddressStatistics
+	# timestamp
+	TimestampCreated::Int32
+	TimestampLastActive::Int32
+	TimestampLastReceived::Int32
+	TimestampLastPayed::Int32
+	# amount
+	AmountIncomeTotal::Float64
+	AmountExpenseTotal::Float64
+	# statistics
+	NumTxInTotal::Int32
+	NumTxOutTotal::Int32
+	# relevant usdt amount
+	UsdtPayed4Input::Float64
+	UsdtReceived4Output::Float64
+	AveragePurchasePrice::Float32
+	LastSellPrice::Float32
+	# calculated extra
+	UsdtNetRealized::Float64
+	UsdtNetUnrealized::Float64
+	Balance::Float64
+	end
+function GenerateState(addrId::UInt32, startN::Int, endN::Int)::AddressStatistics
+	mintTags  = map(x->x>0.0, df.Amount[startN:endN])
+	spentTags = map(x->x<0.0, df.Amount[startN:endN])
+	ret   = AddressStatistics(
+		min(df.Timestamp[startN:endN]...), # TimestampCreated Int32
+		max(df.Timestamp[startN:endN]...), # TimestampLastActive Int32
+		df.Timestamp[
+			startN - 1 + findlast(x->x, mintTags)
+		], # TimestampLastReceived Int32
+		df.Timestamp[
+			startN - 1 + findlast(x->x, spentTags)
+		], # TimestampLastPayed Int32
+		df.Amount[startN:endN][mintTags] |> sum, # AmountIncomeTotal Float64
+		df.Amount[startN:endN][spentTags] |> sum |> abs, # AmountExpenseTotal Float64
+		sum(mintTags), # NumTxInTotal Int32
+		sum(spentTags), # NumTxOutTotal Int32
+		0, # UsdtPayed4Input Float64
+		0, # UsdtReceived4Output Float64
+		0, # AveragePurchasePrice Float32
+		0, # LastSellPrice Float32
+		0, # UsdtNetRealized Float64
+		0, # UsdtNetUnrealized Float64
+		0 , # Balance Float64
+	)
+	return ret
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 nextPosRef = 1
 currentPos = 1
 addrId = df.AddressId[currentPos]
