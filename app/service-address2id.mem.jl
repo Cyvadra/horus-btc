941796bb2b6dcd5809254208368fcd2d76d63655
邓捷 <dj@lifesdk.com>
2022-03-03 15:18:49 +0800

    Add auto save/load; optimizations etc.

--- app/service-address2id.mem.jl
+++ app/service-address2id.mem.jl
@@ -1,23 +1,37 @@
 using DataStructures
 using ThreadSafeDicts # private repo
+using JLD2
 
 # Base Config
 	const DATA_DIR = "/mnt/data/AddressServiceTrie/"
 
-# Config: CURRENT_MAX_N
+# AddressCounter
   CURRENT_MAX_N = UInt32(0)
+  # auto load
   if filesize(DATA_DIR*"CURRENT_MAX_N") > 0
   	CURRENT_MAX_N = parse(UInt32, readline(DATA_DIR*"CURRENT_MAX_N"))
 	  end
-	struct AddressCounterTpl
+	mutable struct AddressCounterTpl
 		dlock::Threads.SpinLock
 		x::UInt32
 		end
 	AddressCounter = AddressCounterTpl(Threads.SpinLock(), CURRENT_MAX_N)
+
+# Auto Save
   function WriteCounter()
   	write(DATA_DIR*"CURRENT_MAX_N", string(AddressCounter.x))
 	  end
+	function WriteTrieFile()
+		@info "Writing Addresss Trie into $DATA_DIR..."
+		@info "DO NOT TERMINATE NOW"
+		JLD2.save(DATA_DIR*"Trie.RootP2PKH.jld2", "RootP2PKH", RootP2PKH)
+		JLD2.save(DATA_DIR*"Trie.RootP2SH.jld2", "RootP2SH", RootP2SH)
+		JLD2.save(DATA_DIR*"Trie.RootBech32.jld2", "RootBech32", RootBech32)
+		JLD2.save(DATA_DIR*"Trie.RootOther.jld2", "RootOther", RootOther)
+		@info "Synchronization Complete!"
+		end
 	atexit(WriteCounter)
+	atexit(WriteTrieFile)
 
 # Defaults
 	const NUM_NOT_EXIST = UInt32(0)
@@ -30,6 +44,13 @@ using ThreadSafeDicts # private repo
 	const firstLetterP2PKH  = '1'
 	const firstLetterP2SH   = '3'
 	const firstLetterBech32 = 'b'
+	# auto load
+	if filesize(DATA_DIR*"Trie.RootOther.jld2") > 0
+		RootP2PKH  = JLD2.load(DATA_DIR*"Trie.RootP2PKH.jld2", "RootP2PKH")
+		RootP2SH   = JLD2.load(DATA_DIR*"Trie.RootP2SH.jld2", "RootP2SH")
+		RootBech32 = JLD2.load(DATA_DIR*"Trie.RootBech32.jld2", "RootBech32")
+		RootOther  = JLD2.load(DATA_DIR*"Trie.RootOther.jld2", "RootOther")
+	end
 
 # Methods
 	function ReadID(addr::AbstractString)::UInt32
@@ -68,8 +89,8 @@ using ThreadSafeDicts # private repo
 			tmpRet = get(RootP2PKH, addr[2:end], NUM_NOT_EXIST)
 			if iszero(tmpRet)
 				lock(AddressCounter.dlock)
-				AddressCounter.x = AddressCounter.x + 1
-				tmpRet = AddressCounter.x
+				AddressCounter.x += 1
+				tmpRet += AddressCounter.x
 				RootP2PKH[addr[2:end]] = tmpRet
 				unlock(AddressCounter.dlock)
 			end
@@ -77,8 +98,8 @@ using ThreadSafeDicts # private repo
 			tmpRet = get(RootP2SH, addr[2:end], NUM_NOT_EXIST)
 			if iszero(tmpRet)
 				lock(AddressCounter.dlock)
-				AddressCounter.x = AddressCounter.x + 1
-				tmpRet = AddressCounter.x
+				AddressCounter.x += 1
+				tmpRet += AddressCounter.x
 				RootP2SH[addr[2:end]] = tmpRet
 				unlock(AddressCounter.dlock)
 			end
@@ -86,18 +107,18 @@ using ThreadSafeDicts # private repo
 			tmpRet = get(RootBech32, addr[5:end], NUM_NOT_EXIST)
 			if iszero(tmpRet)
 				lock(AddressCounter.dlock)
-				AddressCounter.x = AddressCounter.x + 1
-				tmpRet = AddressCounter.x
+				AddressCounter.x += 1
+				tmpRet += AddressCounter.x
 				RootBech32[addr[5:end]] = tmpRet
 				unlock(AddressCounter.dlock)
 			end
 		elseif haskey(RootOther, addr)
-			tmpRet = RootOther[addr]
+			tmpRet += RootOther[addr]
 		else
 			lock(AddressCounter.dlock)
-			AddressCounter.x = AddressCounter.x + 1
-			tmpRet = AddressCounter.x
-			RootOther[addr] = tmpRet
+			AddressCounter.x +=  1
+			RootOther[addr] = AddressCounter.x
+			tmpRet += AddressCounter.x
 			unlock(AddressCounter.dlock)
 		end
 		return tmpRet
