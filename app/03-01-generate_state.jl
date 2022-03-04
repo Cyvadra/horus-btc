f91b771a9979d33b68faa7492c57523141ab1c31
邓捷 <dj@lifesdk.com>
2022-03-04 20:40:23 +0800

    bugfix: listEndPos alignment. notice: ProgressMeter may influence performance in a tiny extent

--- app/03-01-generate_state.jl
+++ app/03-01-generate_state.jl
@@ -102,7 +102,7 @@ _len         = length(uniqueAddrs)
 listStartPos = zeros(Int, _len)
 listEndPos   = collect(1:_len)
 listAddrId   = zeros(UInt32, _len)
-prog = Progress(length(sumAmount)-1)
+prog = Progress(length(uniqueAddrs)-1)
 listStartPos[1]   = findfirst(x->x==uniqueAddrs[1], sumAddrId)
 listEndPos[1]     = findnext(x->x!==uniqueAddrs[1], sumAddrId, 1)
 listAddrId[1]     = uniqueAddrs[1]
@@ -113,7 +113,7 @@ Threads.@threads for i in 2:_len
 		listEndPos[i-1]
 	)
 	listStartPos[i] = tmpStartPos
-	listEndPos[i]   = findnext(x->x!==uniqueAddrs[i], sumAddrId, tmpStartPos)
+	listEndPos[i]   = findnext(x->x!==uniqueAddrs[i], sumAddrId, tmpStartPos) - 1
 	listAddrId[i]   = uniqueAddrs[i]
 	next!(prog)
 end
