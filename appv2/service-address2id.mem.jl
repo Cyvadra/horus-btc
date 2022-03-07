222f1899cb4c7d910272d37fc079072f2546307d
邓捷 <dj@lifesdk.com>
2022-03-07 20:02:04 +0800

    Rewrite in-mem address2id, still not working for uncontrollable gc reason

--- appv2/service-address2id.mem.jl
+++ appv2/service-address2id.mem.jl
@@ -36,15 +36,23 @@ using JLD2
 # Defaults
 	const NUM_NOT_EXIST = UInt32(0)
 
+# Fundamental others
+	struct AddressSet
+		listString::Vector{String}
+		listId::Vector{UInt32}
+		end
+	tplAddressSet = AddressSet(String[], UInt32[])
+
 # Fundamental Trie Trees
-	RootP2PKH  = Trie{UInt32}()
-	RootP2SH   = Trie{UInt32}()
-	RootBech32 = Trie{UInt32}()
+	RootP2PKH  = Trie{AddressSet}()
+	RootP2SH   = Trie{AddressSet}()
+	RootBech32 = Trie{AddressSet}()
 	RootOther  = ThreadSafeDict{String, UInt32}()
 	const firstLetterP2PKH  = '1'
 	const firstLetterP2SH   = '3'
 	const firstLetterBech32 = 'b'
-	# auto load
+
+# auto load
 	if filesize(DATA_DIR*"Trie.RootOther.jld2") > 0
 		RootP2PKH   = JLD2.load(DATA_DIR*"Trie.RootP2PKH.jld2", "RootP2PKH")
 		RootP2SH    = JLD2.load(DATA_DIR*"Trie.RootP2SH.jld2", "RootP2SH")
@@ -56,11 +64,17 @@ using JLD2
 	function ReadID(addr::AbstractString)::UInt32
 		firstChar = addr[1]
 		if firstChar == firstLetterP2PKH
-			return get(RootP2PKH, addr[2:end], NUM_NOT_EXIST)
+			s = get(RootP2PKH, addr[2:5], NUM_NOT_EXIST)
+			x = findfirst(x->x==addr[6:end], s.listString)
+			isnothing(x) ? NUM_NOT_EXIST : s.listId[x]
 		elseif firstChar == firstLetterP2SH
-			return get(RootP2SH, addr[2:end], NUM_NOT_EXIST)
+			s = get(RootP2SH, addr[2:5], NUM_NOT_EXIST)
+			x = findfirst(x->x==addr[6:end], s.listString)
+			isnothing(x) ? NUM_NOT_EXIST : s.listId[x]
 		elseif firstChar == firstLetterBech32
-			return get(RootBech32, addr[5:end], NUM_NOT_EXIST)
+			s = get(RootBech32, addr[5:9], NUM_NOT_EXIST)
+			x = findfirst(x->x==addr[10:end], s.listString)
+			isnothing(x) ? NUM_NOT_EXIST : s.listId[x]
 		else
 			if haskey(RootOther, addr)
 				return RootOther[addr]
@@ -72,57 +86,105 @@ using JLD2
 	function SetID(addr::AbstractString, id)::Nothing
 		firstChar = addr[1]
 		if firstChar == firstLetterP2PKH
-			RootP2PKH[addr[2:end]] = id
+			s = get(RootP2PKH, addr[2:5], nothing)
+			if isnothing(s)
+				RootP2PKH[addr[2:5]] = AddressSet(String[], UInt32[])
+				s = RootP2PKH[addr[2:5]]
+			end
+			x = findfirst(x->x==addr[6:end], s.listString)
+			if isnothing(x)
+				push!(RootP2PKH[addr[2:5]].listString, addr[6:end])
+				push!(RootP2PKH[addr[2:5]].listId, id)
+			end
 		elseif firstChar == firstLetterP2SH
-			RootP2SH[addr[2:end]] = id
+			s = get(RootP2SH, addr[2:5], nothing)
+			if isnothing(s)
+				RootP2SH[addr[2:5]] = AddressSet(String[], UInt32[])
+				s = RootP2SH[addr[2:5]]
+			end
+			x = findfirst(x->x==addr[6:end], s.listString)
+			if isnothing(x)
+				push!(RootP2SH[addr[2:5]].listString, addr[6:end])
+				push!(RootP2SH[addr[2:5]].listId, id)
+			end
 		elseif firstChar == firstLetterBech32
-			RootBech32[addr[5:end]] = id
+			s = get(RootBech32, addr[5:9], nothing)
+			if isnothing(s)
+				RootBech32[addr[5:9]] = AddressSet(String[], UInt32[])
+				s = RootBech32[addr[5:9]]
+			end
+			x = findfirst(x->x==addr[10:end], s.listString)
+			if isnothing(x)
+				push!(RootBech32[addr[5:9]].listString, addr[10:end])
+				push!(RootBech32[addr[5:9]].listId, id)
+			end
 		else
 			RootOther[addr] = id
-		end
-		if id > AddressCounter.x
-			AddressCounter.x = id
 		end
 		return nothing
 		end
 	function GenerateID(addr::AbstractString)::UInt32
 		firstChar = addr[1]
-		tmpRet = UInt32(0)
+		tmpRet = UInt32(1)
 		if firstChar == firstLetterP2PKH
-			tmpRet = get(RootP2PKH, addr[2:end], NUM_NOT_EXIST)
-			if iszero(tmpRet)
+			s = get(RootP2PKH, addr[2:5], nothing)
+			if isnothing(s)
+				RootP2PKH[ addr[2:5] ] = AddressSet(String[], UInt32[])
+				s = RootP2PKH[ addr[2:5] ]
+			end
+			x = findfirst(x->x==addr[6:end], s.listString)
+			if isnothing(x)
 				lock(AddressCounter.dlock)
-				AddressCounter.x += 1
 				tmpRet += AddressCounter.x
-				RootP2PKH[addr[2:end]] = tmpRet
+				AddressCounter.x = tmpRet
 				unlock(AddressCounter.dlock)
+				push!(RootP2PKH[addr[2:5]].listId, tmpRet)
+				push!(RootP2PKH[addr[2:5]].listString, addr[6:end])
+			else
+				tmpRet = s.listId[x]
 			end
 		elseif firstChar == firstLetterP2SH
-			tmpRet = get(RootP2SH, addr[2:end], NUM_NOT_EXIST)
-			if iszero(tmpRet)
+			s = get(RootP2SH, addr[2:5], nothing)
+			if isnothing(s)
+				RootP2SH[ addr[2:5] ] = AddressSet(String[], UInt32[])
+				s = RootP2SH[ addr[2:5] ]
+			end
+			x = findfirst(x->x==addr[6:end], s.listString)
+			if isnothing(x)
 				lock(AddressCounter.dlock)
-				AddressCounter.x += 1
 				tmpRet += AddressCounter.x
-				RootP2SH[addr[2:end]] = tmpRet
+				AddressCounter.x = tmpRet
 				unlock(AddressCounter.dlock)
+				push!(RootP2SH[addr[2:5]].listId, tmpRet)
+				push!(RootP2SH[addr[2:5]].listString, addr[6:end])
+			else
+				tmpRet = s.listId[x]
 			end
 		elseif firstChar == firstLetterBech32
-			tmpRet = get(RootBech32, addr[5:end], NUM_NOT_EXIST)
-			if iszero(tmpRet)
+			s = get(RootBech32, addr[5:9], nothing)
+			if isnothing(s)
+				RootBech32[ addr[5:9] ] = AddressSet(String[], UInt32[])
+				s = RootBech32[ addr[5:9] ]
+			end
+			x = findfirst(x->x==addr[10:end], s.listString)
+			if isnothing(x)
 				lock(AddressCounter.dlock)
-				AddressCounter.x += 1
 				tmpRet += AddressCounter.x
-				RootBech32[addr[5:end]] = tmpRet
+				AddressCounter.x = tmpRet
 				unlock(AddressCounter.dlock)
+				push!(RootBech32[addr[5:9]].listId, tmpRet)
+				push!(RootBech32[addr[5:9]].listString, addr[10:end])
+			else
+				tmpRet = s.listId[x]
 			end
 		elseif haskey(RootOther, addr)
-			tmpRet += RootOther[addr]
+			tmpRet = RootOther[addr]
 		else
 			lock(AddressCounter.dlock)
-			AddressCounter.x +=  1
-			RootOther[addr] = AddressCounter.x
 			tmpRet += AddressCounter.x
+			AddressCounter.x = tmpRet
 			unlock(AddressCounter.dlock)
+			RootOther[addr] = tmpRet
 		end
 		return tmpRet
 		end
