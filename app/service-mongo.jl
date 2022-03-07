9d1f43f07aa0ba688df7b59e30ae858669d1ae50
邓捷 <dj@lifesdk.com>
2022-03-07 14:03:49 +0800

    Separate mongo service

---
+++ app/service-mongo.jl
@@ -0,0 +1,143 @@
+using BSON, Mongoc
+using Dates
+using JSON
+
+dataFolder   = "/mnt/data/cacheTx/"
+shuffleRng   = Random.MersenneTwister(10086)
+
+# Init Mongo
+client = Mongoc.Client("mongodb://localhost:27017")
+@show Mongoc.ping(client)
+db     = client["bitcore"]
+
+# Mongo Connection Pool
+mongoClients = [ Mongoc.Client("mongodb://localhost:27017") for i in 1:Threads.nthreads() ]
+mongoDBs    = map(x->x["bitcore"], mongoClients)
+@show collect(Mongoc.find_collections(db))
+function MongoCollection(key::String)::Mongoc.Collection
+	return mongoDBs[Threads.threadid()][key]
+	end
+
+# Middlewares: Get data from mongodb
+function GetBlockInfo(height)::Mongoc.BSON
+	return Mongoc.find_one(MongoCollection("blocks"),  Mongoc.BSON("{\"height\":$height}"))
+	end
+function GetBlockTransactions(height::Int)::Vector{Mongoc.BSON}
+	block     = Mongoc.find_one(MongoCollection("blocks"),  Mongoc.BSON("{\"height\":$height}"))
+	blockHash = block["hash"]
+	res       = collect( Mongoc.find(MongoCollection("transactions"), Mongoc.BSON("{\"blockHash\":\"$blockHash\"}")) )
+	if !isequal(length(res), block["transactionCount"])
+		@warn "Block $height error!"
+		push!(FailedBlockNums, Int64(height))
+	end
+	return res
+	end
+	FailedBlockNums = Int[]
+function GetCoinsByTxid(txid::String)::Vector{Mongoc.BSON}
+	a = collect( Mongoc.find(MongoCollection("coins"), Mongoc.BSON("{\"mintTxid\":\"$txid\"}")) )
+	append!(a, collect( Mongoc.find(MongoCollection("coins"), Mongoc.BSON("{\"spentTxid\":\"$txid\"}")) ) )
+	end
+function GetCoinsByMintHeight(height)::Vector{Mongoc.BSON}
+	collect( Mongoc.find(MongoCollection("coins"), Mongoc.BSON("{\"mintHeight\":\"$height\"}")) )
+	end
+function GetCoinsInputByTxid(txid::String)::Vector{Mongoc.BSON}
+	filter!(
+		x->x["value"] > 0,
+		collect( Mongoc.find(MongoCollection("coins"), Mongoc.BSON("{\"spentTxid\":\"$txid\"}")) )
+		)
+	end
+function GetCoinsOutputByTxid(txid::String, height::Int)::Vector{Mongoc.BSON}
+	filter!(
+		x->x["spentHeight"] <= height && x["value"] > 0,
+		collect(
+			Mongoc.find(MongoCollection("coins"), Mongoc.BSON("{\"mintTxid\":\"$txid\"}"))
+			)
+		)
+	end
+function GetCoinsByAddress(addr::String)::Vector{Mongoc.BSON}
+	collect( Mongoc.find(MongoCollection("coins"), Mongoc.BSON("{\"address\":\"$addr\"}")) )
+	end
+
+# Basic: Global result procedure
+function bitcoreInt2Float64(i)::Float64
+	Float64(i) / 1e8
+	end
+function bitcoreInt2Float32(i)::Float32
+	Float32(i) / 1e8
+	end
+function str2unixtime(str::String)::Int64
+	dt = Dates.DateTime(str, dateformat"yyyy-mm-ddTHH:MM:SS.s\Z")
+	return round(Int64, datetime2unix(dt))
+	end
+
+# Union
+function GetAddressCoins(addr::String)::Vector{Mongoc.BSON}
+	tmpBson = Mongoc.BSON("""{
+		"chain":"BTC", "network":"mainnet", "address":"$addr"
+		}""")
+	collect(Mongoc.find(db["coins"], tmpBson))
+	end
+function GetBlockCoins(height::Int)::Vector{Mongoc.BSON}
+	txs       = GetBlockTransactions(height)
+	# timeStamp = datetime2unix(txs[1]["blockTime"])
+	retList   = Vector{Mongoc.BSON}()
+	tmpList = Vector{Mongoc.BSON}()
+	for tx in txs
+		inputs  = GetCoinsInputByTxid(string(tx["txid"]))
+		outputs = GetCoinsOutputByTxid(string(tx["txid"]), height)
+		append!(tmpList, inputs)
+		append!(tmpList, outputs)
+		Random.shuffle!(shuffleRng, tmpList)
+		append!(retList, tmpList)
+		empty!(tmpList)
+	end
+	return retList
+	end
+
+# Transfer into Vector{cacheTx}
+struct cacheTx
+	addressId::UInt32
+	amount::Float64
+	timestamp::Int32
+	end
+function ProcessBlockN(height::Int)::Vector{cacheTx}
+	txs           = GetBlockTransactions(height)
+	timeStamp     = round(Int, datetime2unix(txs[1]["blockTime"]))
+	retList   = Vector{cacheTx}()
+	tmpList   = Vector{cacheTx}()
+	tmpDict   = Dict{String,UInt32}()
+	for tx in txs
+		inputs  = GetCoinsInputByTxid(string(tx["txid"]))
+		outputs = GetCoinsOutputByTxid(string(tx["txid"]), height)
+		addrs   = unique(map(x->x["address"], vcat(inputs, outputs)))
+		for addr in addrs
+			if !haskey(tmpDict, addr)
+				tmpDict[addr] = GenerateID(addr)
+			end
+		end
+		for c in inputs
+			push!(tmpList, cacheTx(
+				tmpDict[c["address"]],
+				-bitcoreInt2Float64(c["value"]),
+				timeStamp
+				))
+		end
+		for c in outputs
+			push!(tmpList, cacheTx(
+				tmpDict[c["address"]],
+				bitcoreInt2Float64(c["value"]),
+				timeStamp
+				))
+		end
+		Random.shuffle!(shuffleRng, tmpList)
+		append!(retList, tmpList)
+		empty!(tmpList)
+	end
+	empty!(tmpDict)
+	JLD2.save(dataFolder * "$height.jld2", "txList", retList)
+	return retList
+	end
+
+
+
+
