668402d01f625e2e138ce35b1a0eb1e1a77490b2
邓捷 <dj@lifesdk.com>
2022-03-07 20:59:21 +0800

    whatever..

--- app/service-address2id.traditional.jl
+++ app/service-address2id.traditional.jl
@@ -1,18 +1,18 @@
-using ThreadSafeDicts
 
-AddressStringDict = ThreadSafeDict{String,UInt32}()
+using ProgressMeter
+
+AddressStringDict = Dict{String,UInt32}()
 AddressStringDict["maximum"] = 1
-
-AddressStringDict.enabled = false
 
 tmpList = []
 counter = 0
-@showprogress for i in 1:1600:length(f)
-	counter = i+1599
-	tmpList = map(x->split(x,'\t'), f[i:counter])
-	for s in tmpList
-		AddressStringDict[s[1]] = parse(UInt32, s[2])
+_len = length(f)
+s = ["asdf", "123"]
+@showprogress for l in f
+	s = split(l,'\t')
+	AddressStringDict[s[1]] = parse(UInt32, s[2])
 	end
-end
+
+
 
 
