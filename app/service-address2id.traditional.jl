edd5f653a05302fdc13ecdb18a484f24182309b4
邓捷 <dj@lifesdk.com>
2022-03-08 15:25:29 +0800

    Restore initail version for current MVP use

--- app/service-address2id.traditional.jl
+++ app/service-address2id.traditional.jl
@@ -2,17 +2,35 @@
 using ProgressMeter
 
 AddressStringDict = Dict{String,UInt32}()
-AddressStringDict["maximum"] = 1
+const NUM_NOT_EXIST = UInt32(0)
+const TAG_MAX = "maximum"
+AddressStringDict[TAG_MAX] = 930830585
+AddressStringLock = Threads.SpinLock()
 
-tmpList = []
-counter = 0
-_len = length(f)
-s = ["asdf", "123"]
-@showprogress for l in f
-	s = split(l,'\t')
-	AddressStringDict[s[1]] = parse(UInt32, s[2])
+# Data Loader
+# tmpList = []
+# counter = 0
+# _len = length(f)
+# s = ["asdf", "123"]
+# @showprogress for l in f
+# 	s = split(l,'\t')
+# 	AddressStringDict[s[1]] = parse(UInt32, s[2])
+# 	end
+
+function ReadID(addr::AbstractString)::UInt32
+	get(AddressStringDict, addr, NUM_NOT_EXIST)
 	end
 
-
-
+function GenerateID(addr::AbstractString)::UInt32
+	if !haskey(AddressStringDict, addr)
+		lock(AddressStringLock)
+		n = UInt32(AddressStringDict[TAG_MAX] + 1)
+		AddressStringDict[addr] = n
+		AddressStringDict[TAG_MAX] = n
+		unlock(AddressStringLock)
+		return n
+	else
+		return AddressStringDict[addr]
+	end
+	end
 
