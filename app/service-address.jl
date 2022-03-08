905df13a5955e56816b50e7ece3d10bf29eaaa8d
邓捷 <dj@lifesdk.com>
2022-03-08 19:58:27 +0800

    append another version of string2id to current service-address.jl

--- app/service-address.jl
+++ app/service-address.jl
@@ -34,12 +34,52 @@ function isNew(ids::Vector{UInt32})::Vector{Bool}
 	return AddressService.mapFunctionOnFieldIds(iszero, :TimestampCreated, ids)
 	end
 
+#=========================================# 
 
+const NUM_NOT_EXIST = UInt32(0)
+const TAG_MAX = "maximum"
+# TAG_MAX = 930830585
+AddressStringLock = Threads.SpinLock()
+AddressIOBufferDict = Dict{UInt8, IOBuffer}()
 
+function ReadID(addr::AbstractString)::UInt32
+	addrv = transcode(UInt8, addr)
+	_len  = UInt8(length(addrv))
+	if addrv[1] == 0x31
+		io = AddressIOBufferDict[_len]
+		v = readuntil(io, addrv[2:end])
+		if length(v) > 0
+			skip(io, 1)
+			return read(io, UInt32)
+		end
+	# elseif addrv[1] == 0x33
+	# 	v = readuntil(AddressIOBufferDict(length(addrv)), addrv)
+	# elseif addrv[1] == 0x62
+	# 	v = readuntil(AddressIOBufferDict(length(addrv)), addrv)
+	end
+	return NUM_NOT_EXIST
+	end
 
-
-
-
+function SetID(addr::AbstractString, id::UInt32)::Nothing
+	addrv = transcode(UInt8, addr)
+	_len  = UInt8(length(addrv))
+	if addrv[1] == 0x31
+		if !haskey(AddressIOBufferDict, _len)
+			AddressIOBufferDict[_len] = IOBuffer()
+		end
+		io = AddressIOBufferDict[_len]
+		v = readuntil(io, addrv[2:end])
+		if length(v) > 0
+			skip(io, 1)
+			write(io, id)
+			return nothing
+		else
+			write(io, addrv[2:end])
+			write(io, zero(UInt8))
+			return nothing
+		end
+	end
+	end
 
 
 
