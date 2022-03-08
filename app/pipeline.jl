921ff05188d4fc2c071049248d41b8091dda9bc3
邓捷 <dj@lifesdk.com>
2022-03-08 16:05:28 +0800

    init pipeline

--- app/pipeline.jl
+++ app/pipeline.jl
@@ -1,10 +1,18 @@
 
+include("./service-address.ng.jl");
+include("./service-address2id.traditional.jl");
+include("./service-FinanceDB.jl");
+include("./service-mongo.jl");
+include("./service-block_timestamp.jl");
+include("./middleware-calc_addr_diff.jl");
 
 
+lastProcessedBlockN = Timestamp2LastBlockN(1642865146)
 
-
-
-
+arrayDiff = Address2StateDiff(lastProcessedBlockN+1,lastProcessedBlockN+1)
+currentTs = round(Int,
+		(Dates.now() - Hour(24)) |> datetime2unix)
+MergeAddressState!(arrayDiff, GetBTCPriceWhen(currentTs))
 
 
 
