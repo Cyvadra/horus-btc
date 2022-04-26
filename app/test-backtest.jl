194b409890be42e75bf5a0324672acb6ce96eacf
邓捷 <dj@lifesdk.com>
2022-04-26 21:59:36 +0800

    struct backtest

--- app/test-backtest.jl
+++ app/test-backtest.jl
@@ -1,11 +1,18 @@
 
+include("./config.jl");
+include("./service-block_timestamp.jl");
 include("./middleware-results-flexible.jl");
+include("./functions-generate_window.jl");
 
 TableResults.Open(true)
+@show GetLastResultsID()
 
-GetLastResultsID()
+using Dates
 
+fromDate = DateTime(2019,3,1,1)
+toDate   = DateTime(2022,3,31,1)
 
+GenerateWindowedViewH2(fromDate, toDate)
 
 
 
