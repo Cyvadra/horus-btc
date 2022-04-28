52bd032d2f4019c32ce3c6a02b2ae449543a847f
邓捷 <dj@lifesdk.com>
2022-04-28 10:42:57 +0800

    add functions ma

---
+++ app/functions-ma.jl
@@ -0,0 +1,49 @@
+using Statistics
+
+function sma(v::Vector{T}, numMa::Int)::Vector where T<:Real
+	if length(v) < 3numMa
+		throw("insufficient data")
+	end
+	vRet = zeros(length(v))
+	vRet[1] = v[1]
+	for i in 2:numMa+1
+		vRet[i] = Statistics.mean(v[1:i-1])
+	end
+	for i in numMa+2:length(v)
+		vRet[i] = (numMa * vRet[i-1] - v[i-numMa-1] + v[i-1]) / numMa
+	end
+	return vRet
+	end
+
+function ma(v::Vector{T}, numMa::Int)::Vector where T<:Real
+	vRet = zeros(length(v))
+	vRet[1] = v[1]
+	for i in 2:length(v)
+		vRet[i] = ((numMa-1)*vRet[i-1] + v[i-1]) / numMa
+	end
+	return vRet
+	end
+
+function ema(v::Vector{T}, numMa::Int)::Vector where T<:Real
+	vRet = zeros(length(v))
+	vRet[1] = v[1]
+	for i in 2:length(v)
+		vRet[i] = ((numMa-1)*vRet[i-1] + 2v[i-1]) / (numMa+1)
+	end
+	return vRet
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
