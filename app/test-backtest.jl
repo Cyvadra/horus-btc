72a58bcc020296418b28f39f3e923e3c910a62f4
邓捷 <dj@lifesdk.com>
2022-04-28 19:59:12 +0800

    generate standardized sequences

--- app/test-backtest.jl
+++ app/test-backtest.jl
@@ -1,28 +1,47 @@
 
 include("./config.jl");
+include("./utils.jl");
 include("./cache-generated.jl");
 include("./service-block_timestamp.jl");
 include("./middleware-results-flexible.jl");
 include("./functions-generate_window.jl");
 include("./functions-ma.jl");
+include("./client-config.jl");
 
 using Dates
 
 TableResults.Open(true)
 @show GetLastResultsID()
 
-fromDate = DateTime(2019,3,1,1)
-toDate   = DateTime(2022,3,31,1)
+numMa    = 12 # 24h
+tmpSyms  = ResultCalculations |> fieldnames |> collect
 
-GenerateWindowedViewH2(fromDate, toDate)
+function GenerateSequences(tmpRet::Vector{ResultCalculations})::Matrix{Float64}
+	anoRet   = Dict{String,Vector}()
+	for s in tmpSyms
+		anoRet[string(s)] = map(x->getfield(x,s), tmpRet)
+	end
+	baseList  = anoRet["amountTotalTransfer"]
+	sequences = Vector[]
+	for k in dnnList
+		tmpList = anoRet[k] ./ baseList
+		tmpBase = ma(tmpList, numMa)
+		tmpList = meanfit(tmpList ./ tmpBase)
+		push!(sequences,
+			tmpList
+			)
+	end
+	return hcat(sequences...)
+	end
 
-
-
-
-
-
-
-
+fromDate  = DateTime(2019,3,1,0)
+toDate    = DateTime(2022,3,31,0)
+@time res = GenerateWindowedViewH3(fromDate, toDate) |> GenerateSequences
+for i in 1:5
+	res = vcat(res,
+		GenerateWindowedViewH3(fromDate + Minute(30i), toDate + Minute(30i)) |> GenerateSequences
+		)
+end
 
 
 
