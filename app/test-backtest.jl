02a40c96cc3b44e79f34d00d3c599551a144dc85
邓捷 <dj@lifesdk.com>
2022-04-28 11:19:50 +0800

    use cache structure for backtest.jl

--- app/test-backtest.jl
+++ app/test-backtest.jl
@@ -1,13 +1,15 @@
 
 include("./config.jl");
+include("./cache-generated.jl");
 include("./service-block_timestamp.jl");
 include("./middleware-results-flexible.jl");
 include("./functions-generate_window.jl");
+include("./functions-ma.jl");
+
+using Dates
 
 TableResults.Open(true)
 @show GetLastResultsID()
-
-using Dates
 
 fromDate = DateTime(2019,3,1,1)
 toDate   = DateTime(2022,3,31,1)
