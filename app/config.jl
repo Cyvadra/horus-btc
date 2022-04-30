ef327c641375272fa4df09687d5fccd33227b741
邓捷 <dj@lifesdk.com>
2022-04-30 13:57:15 +0800

    extract root data path

--- app/config.jl
+++ app/config.jl
@@ -1,10 +1,12 @@
 
-tsFile            = "/mnt/data/bitcore/BlockTimestamps.dict.jld2"
+rootPath          = "/mnt/data"
+
+tsFile            = "$rootPath/bitcore/BlockTimestamps.dict.jld2"
 cacheMarket       = "/tmp/market.json"
-folderAddressDB   = "/mnt/data/AddressServiceDB-v2/"
-fileAddressString = "/mnt/data/bitcore/addr.latest.txt"
-folderMarket      = "/mnt/data/BTC_USDT_1m/"
-folderResults     = "/mnt/data/results-flexible/"
+folderAddressDB   = "$rootPath/AddressServiceDB-v2/"
+fileAddressString = "$rootPath/bitcore/addr.latest.txt"
+folderMarket      = "$rootPath/BTC_USDT_1m/"
+folderResults     = "$rootPath/results-flexible/"
 
 
 
