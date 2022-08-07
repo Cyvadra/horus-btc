26d5772e9facd2dba989bed16bd4b8a81a80bc4e
邓捷 <dj@lifesdk.com>
2022-08-07 16:54:49 +0800

    init strategy engine

---
+++ app/service-strategy_engine.jl
@@ -0,0 +1,51 @@
+
+using Statistics
+
+mutable struct FieldFeatures
+	middle::Float64
+	mean::Float64
+	high::Float64
+	low::Float64
+	ma3::Float64
+	ma5::Float64
+	ma10::Float64
+	ma15::Float64
+	ma20::Float64
+	ma30::Float64
+	ema3::Float64
+	ema5::Float64
+	ema10::Float64
+	ema15::Float64
+	ema20::Float64
+	ema30::Float64
+	timestamp::Int64
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
