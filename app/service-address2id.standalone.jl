a3fb19f6c846f5b3f2548e849117d4645d8d475d
邓捷 <dj@lifesdk.com>
2022-09-13 12:21:20 +0800

    rewrite address2id module, use in-memory hash table for less mem occupation

--- app/service-address2id.standalone.jl
+++ app/service-address2id.standalone.jl
@@ -1,79 +1,47 @@
 
 using ProgressMeter
 using Dates
+using CRC
 
-AddressStringDict = Dict{String,UInt32}()
+@info "Please mannually drop caches for better memory performance!"
+@info "echo 3 > /proc/sys/vm/drop_caches"
+@info "Press Enter to continue."
+readline();
 const NUM_NOT_EXIST = UInt32(0)
-const TAG_MAX = "maximum"
-AddressStringDict[TAG_MAX] = 1
-AddressStringLock = Threads.SpinLock()
+AddressHashList = fill(NUM_NOT_EXIST, typemax(UInt64))
+AddressMaxId   = UInt32(17)
+AddressDupLock = Threads.SpinLock()
 
-tmpCache = Dict{Bool, String}()
-@info "$(now()) loading address list..."
-f = open(fileAddressString, "r")
-tmpCache[true] = readline(f)
-prog = Progress(1013371292) # modify here
-
-for i in 1:1000 # initial loop for sizehint
-	s = split(tmpCache[true],'\t')
-	tmpVal = parse(UInt32, s[2])
-	if tmpVal > AddressStringDict[TAG_MAX]
-		AddressStringDict[TAG_MAX] = tmpVal
-	end
-	AddressStringDict[s[1]] = tmpVal
-	tmpCache[true] = readline(f)
-	next!(prog)
-	end
-sizehint!(AddressStringDict, round(Int, 1.28e9))
-
-while length(tmpCache[true])>0
-	s = split(tmpCache[true],'\t')
-	tmpVal = parse(UInt32, s[2])
-	if tmpVal > AddressStringDict[TAG_MAX]
-		AddressStringDict[TAG_MAX] = tmpVal
-	end
-	AddressStringDict[s[1]] = tmpVal
-	tmpCache[true] = readline(f)
-	next!(prog)
-	end
-@info "$(now()) loaded, doing gc"
-close(f)
-tmpCache = nothing
-f = nothing
-GC.gc(true)
-@info "$(now()) gc complete" 
-
-
+c64 = CRC.crc(CRC_64)
 
 function ReadID(addr::AbstractString)::UInt32
-	get(AddressStringDict, addr, NUM_NOT_EXIST)
+	return AddressHashList[c64(addr)]
 	end
 
 function GenerateID(addr::AbstractString)::UInt32
-	lock(AddressStringLock)
-	if !haskey(AddressStringDict, addr)
-		n = UInt32(AddressStringDict[TAG_MAX] + 1)
-		AddressStringDict[addr] = n
-		AddressStringDict[TAG_MAX] = n
-		# write(runtimeAddressFile, "$addr\t$n\n")
-		unlock(AddressStringLock)
+	tmpCRC = c64(addr)
+	tmpN = AddressHashList[tmpCRC]
+	if tmpN == NUM_NOT_EXIST
+		lock(AddressDupLock)
+		n = AddressMaxId + UInt32(1)
+		AddressHashList[tmpCRC] = n
+		unlock(AddressDupLock)
+		WriteAddressLine(addr, n)
 		return n
 	else
-		unlock(AddressStringLock)
-		return AddressStringDict[addr]
+		return tmpN
 	end
 	end
 
-function WriteAddressStringDict(path::AbstractString="/mnt/data/bitcore/addr.autosave.txt")
-	@info "$(now()) Auto saving AddressStringDict..."
-	lock(AddressStringLock)
-	f = open(path, "w")
-	_len = length(AddressStringDict)
-	@showprogress for p in AddressStringDict
-		write(f, "$(p[1])\t$(p[2])\n")
+
+fileAddressLog = open(logAddressString, "w")
+function WriteAddressLine(addr::AbstractString, v::UInt32)::Nothing
+	write(fileAddressLog, "$addr\t$v\n")
+	return nothing
 	end
-	unlock(AddressStringLock)
-	close(f)
-	return filesize(path)
+function FlushAddressLine()::Nothing
+	flush(fileAddressLog)
+	close(fileAddressLog)
+	return nothing
 	end
-# atexit(WriteAddressStringDict)
+atexit(FlushAddressLine)
