a3a2dbbe312bf41fe31980634c70876cdc61fffc
邓捷 <dj@lifesdk.com>
2023-01-09 16:30:42 +0800

    calculate gini index

---
+++ app/isolate-gini.jl
@@ -0,0 +1,52 @@
+include("./utils.jl");
+include("./config.jl");
+include("./service-address2id.jl");
+include("./service-FinanceDB.jl");
+include("./service-mongo.jl");
+
+using JLD2
+BALANCE_CHUNKS = JLD2.load("/mnt/data/bitcore/balance.chunks.jld2")["BALANCE_CHUNKS"]
+
+#= definitions:
+	[ boundary[i], boundary[i+1] )
+=#
+
+# define results
+BalanceCounterMatrix = Matrix{Int32}(undef, 780000, 10000);
+BalanceAmountMatrix = Matrix{Float64}(undef, 780000, 10000);
+BalanceCounterMatrix .= 0
+BalanceAmountMatrix .= 0.0
+AddressBalanceList = zeros(Float64, round(Int, 1.28e9))
+
+# define procedure
+function locateChunk(v::Float64)
+	p = findfirst(x->x>=v, BALANCE_CHUNKS)
+	isnothing(p) ? length(BALANCE_CHUNKS) : p
+	end
+
+@showprogress for n in 1:550000
+	# mint
+	currentCoins = GetCoinsByMintHeight(n)
+	a = ReadID.(map(x->x["address"], currentCoins))
+	b = bitcoreInt2Float64.(map(x->x["value"], currentCoins))
+	tmpChunks = locateChunk.(AddressBalanceList[a])
+	BalanceCounterMatrix[n, tmpChunks] .-= 1
+	BalanceAmountMatrix[n, tmpChunks] .-= AddressBalanceList[a]
+	AddressBalanceList[a] .+= b
+	tmpChunks = locateChunk.(AddressBalanceList[a])
+	BalanceCounterMatrix[n, tmpChunks] .+= 1
+	BalanceAmountMatrix[n, tmpChunks] .+= AddressBalanceList[a]
+	# spent
+	currentCoins = GetCoinsBySpentHeight(n)
+	a = ReadID.(map(x->x["address"], currentCoins))
+	b = bitcoreInt2Float64.(map(x->x["value"], currentCoins))
+	tmpChunks = locateChunk.(AddressBalanceList[a])
+	BalanceCounterMatrix[n, tmpChunks] .-= 1
+	BalanceAmountMatrix[n, tmpChunks] .-= AddressBalanceList[a]
+	AddressBalanceList[a] .-= b # difference here
+	tmpChunks = locateChunk.(AddressBalanceList[a])
+	BalanceCounterMatrix[n, tmpChunks] .+= 1
+	BalanceAmountMatrix[n, tmpChunks] .+= AddressBalanceList[a]
+	end
+
+
