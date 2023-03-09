5a920d54b8bea4fdd87279b5075c634aa14593c0
邓捷 <dj@lifesdk.com>
2023-03-09 21:37:38 +0800

    re-enable function ProcessBlockN

--- app/service-mongo.jl
+++ app/service-mongo.jl
@@ -148,27 +148,33 @@ function GetBlockCoinsInRange(fromBlock::Int, toBlock::Int)::Vector{Mongoc.BSON}
 # Transfer into Vector{cacheTx}
 struct cacheTx
 	addressId::UInt32
+	mintHeight::Int32
+	spentHeight::Int32
 	amount::Float64
-	timestamp::Int32
+	timestamp::UInt32
 	end
 function ProcessBlockN(height::Int)::Vector{cacheTx}
 	txs           = GetBlockTransactions(height)
-	timeStamp     = round(Int, datetime2unix(txs[1]["blockTime"]))
-	retList   = Vector{cacheTx}()
+	timeStamp     = UInt32(round(Int, datetime2unix(txs[1]["blockTime"])))
 	tmpList   = Vector{cacheTx}()
 	tmpDict   = Dict{String,UInt32}()
 	for tx in txs
 		inputs  = GetCoinsInputByTxid(string(tx["txid"]))
 		outputs = GetCoinsOutputByTxid(string(tx["txid"]), height)
-		addrs   = unique(map(x->x["address"], vcat(inputs, outputs)))
+		addrs   = unique(vcat(
+			map(x->x["address"], inputs),
+			map(x->x["address"], outputs),
+			))
 		for addr in addrs
 			if !haskey(tmpDict, addr)
-				tmpDict[addr] = String2ID(addr)
+				tmpDict[addr] = ReadID(addr)
 			end
 		end
 		for c in inputs
 			push!(tmpList, cacheTx(
 				tmpDict[c["address"]],
+				c["mintHeight"],
+				c["spentHeight"],
 				-bitcoreInt2Float64(c["value"]),
 				timeStamp
 				))
@@ -176,18 +182,19 @@ function ProcessBlockN(height::Int)::Vector{cacheTx}
 		for c in outputs
 			push!(tmpList, cacheTx(
 				tmpDict[c["address"]],
+				c["mintHeight"],
+				c["spentHeight"],
 				bitcoreInt2Float64(c["value"]),
 				timeStamp
 				))
 		end
-		append!(retList, tmpList)
-		empty!(tmpList)
 	end
 	empty!(tmpDict)
-	JLD2.save(dataFolder * "$height.jld2", "txList", retList)
-	return retList
+	tmpDict = nothing
+	# JLD2.save(dataFolder * "$height.jld2", "txList", tmpList)
+	return tmpList
 	end
 
 
 
-
+# EOF
