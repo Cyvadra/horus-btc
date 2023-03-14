e6f528e626636e5fa9ce9aca515c8b657a11ce48
邓捷 <dj@lifesdk.com>
2023-03-15 02:32:36 +0800

    auto verify data path

--- app/config.jl
+++ app/config.jl
@@ -1,18 +1,25 @@
 
-rootPath          = "/mnt/data"
+rootPath          = "/mnt/data/horus-storage/rolling"
 ramPath           = "/tmp"
 
 # tsFile            = "$rootPath/bitcore/BlockTimestamps.dict.jld2"
 cacheMarket       = "$ramPath/market.json"
-folderAddressDB   = "$ramPath/AddressServiceDB-v3/"
 # fileAddressString = "$rootPath/bitcore/addr.latest.txt"
 logAddressString  = "$rootPath/addr.runtime.log"
 jldAddressIdFile  = "$rootPath/addr.hashdict.jld2"
 jldAddressBalance = "$rootPath/addr.balance.jld2"
+folderAddressDB   = "$ramPath/AddressServiceDB-v3/"
 folderMarket      = "$rootPath/BTC_USDT_1m/"
 folderResults     = "$rootPath/results-flexible-v3/"
 folderBlockPairs  = "$rootPath/block-timestamp/"
 folderTransactions= "$rootPath/transactions-btc/"
 
-
+@assert isfile( logAddressString )
+@assert isfile( jldAddressIdFile )
+@assert isfile( jldAddressBalance )
+# @assert isdir( folderAddressDB )
+@assert isdir( folderMarket )
+@assert isdir( folderResults )
+@assert isdir( folderBlockPairs )
+@assert isdir( folderTransactions )
 
