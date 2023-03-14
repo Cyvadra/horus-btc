f41339ae7ba20f85bca0fa8b0c40ef5392c6209f
邓捷 <dj@lifesdk.com>
2023-03-14 17:08:48 +0800

    archieve file

---
+++ app/archieved/service-address.v2.jl
@@ -0,0 +1,69 @@
+
+using MmapDB
+
+MmapDB.Init(folderAddressDB)
+
+mutable struct AddressStatistics
+	# timestamp
+	TimestampCreated::Int32
+	TimestampLastActive::Int32
+	TimestampLastReceived::Int32
+	TimestampLastPayed::Int32
+	# amount
+	AmountIncomeTotal::Float64
+	AmountExpenseTotal::Float64
+	# statistics
+	NumTxInTotal::Int32
+	NumTxOutTotal::Int32
+	NumTxTotal::Int32
+	AverageTradeIntervalSecs::Int32
+	# relevant usdt amount
+	UsdtPayed4Input::Float64
+	UsdtReceived4Output::Float64
+	AveragePurchasePrice::Float32
+	LastPurchasePrice::Float32
+	LastSellPrice::Float32
+	# calculated extra
+	UsdtNetRealized::Float64
+	UsdtNetUnrealized::Float64
+	# 5th,Sep. additional fields
+	NumWinning::Int32
+	NumLossing::Int32
+	UsdtAmountWon::Float64
+	UsdtAmountLost::Float64
+	RateWinning::Float32
+	# basic
+	Balance::Float64
+	end
+
+AddressService = MmapDB.GenerateCode(AddressStatistics)
+function isNew(i::UInt32)::Bool
+	return iszero(AddressService.GetFieldTimestampCreated(i))
+	end
+function isNew(ids::Vector{UInt32})::Vector{Bool}
+	return AddressService.mapFunctionOnFieldIds(iszero, :TimestampCreated, ids)
+	end
+
+function GetLastProcessedTimestamp()::Int32
+	tmpVal = AddressService.Findlast(x->!iszero(x), :TimestampLastActive)
+	return max( AddressService.GetFieldTimestampLastActive(tmpVal-3000:tmpVal+100)... )
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/config.jl
+++ app/config.jl
@@ -3,7 +3,7 @@ rootPath          = "/mnt/data"
 
 # tsFile            = "$rootPath/bitcore/BlockTimestamps.dict.jld2"
 cacheMarket       = "/tmp/market.json"
-folderAddressDB   = "$rootPath/AddressServiceDB-v2/"
+folderAddressDB   = "$rootPath/AddressServiceDB-v3/"
 # fileAddressString = "$rootPath/bitcore/addr.latest.txt"
 logAddressString  = "$rootPath/addr.runtime.log"
 jldAddressIdFile  = "$rootPath/addr.hashdict.jld2"
