b33d6cd384e8a5593f6c73aeed9ab08ecae83824
邓捷 <dj@lifesdk.com>
2023-03-14 22:43:17 +0800

    update address db to v3, data files migrated to memory

--- app/config.jl
+++ app/config.jl
@@ -3,7 +3,7 @@ rootPath          = "/mnt/data"
 
 # tsFile            = "$rootPath/bitcore/BlockTimestamps.dict.jld2"
 cacheMarket       = "/tmp/market.json"
-folderAddressDB   = "$rootPath/AddressServiceDB-v3/"
+folderAddressDB   = "/tmp/AddressServiceDB-v3/"
 # fileAddressString = "$rootPath/bitcore/addr.latest.txt"
 logAddressString  = "$rootPath/addr.runtime.log"
 jldAddressIdFile  = "$rootPath/addr.hashdict.jld2"
