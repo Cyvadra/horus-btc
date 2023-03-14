1ef2d03268ea93b5915b534bddbe539d0e6ec07a
邓捷 <dj@lifesdk.com>
2023-03-14 23:51:50 +0800

    modify data path, use new folder to avoid overwriting the old

--- app/config.jl
+++ app/config.jl
@@ -1,14 +1,15 @@
 
 rootPath          = "/mnt/data"
+ramPath           = "/tmp"
 
 # tsFile            = "$rootPath/bitcore/BlockTimestamps.dict.jld2"
-cacheMarket       = "/tmp/market.json"
-folderAddressDB   = "/tmp/AddressServiceDB-v3/"
+cacheMarket       = "$ramPath/market.json"
+folderAddressDB   = "$ramPath/AddressServiceDB-v3/"
 # fileAddressString = "$rootPath/bitcore/addr.latest.txt"
 logAddressString  = "$rootPath/addr.runtime.log"
 jldAddressIdFile  = "$rootPath/addr.hashdict.jld2"
 folderMarket      = "$rootPath/BTC_USDT_1m/"
-folderResults     = "$rootPath/results-flexible/"
+folderResults     = "$rootPath/results-flexible-v3/"
 folderBlockPairs  = "$rootPath/block-timestamp/"
 folderTransactions= "$rootPath/transactions-btc/"
 
